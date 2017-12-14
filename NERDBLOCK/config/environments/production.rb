a742d37bdadd90d11d84ff200bddef062cdb7ca8
UdayShah <udays1995@gmail.com>
2017-12-14 14:34:30 -0500

    idk

--- NERDBLOCK/config/environments/production.rb
+++ NERDBLOCK/config/environments/production.rb
@@ -66,7 +66,7 @@ Rails.application.configure do
   # Set this to true and configure the email server for immediate delivery to raise delivery errors.
   config.action_mailer.raise_delivery_errors = true
   config.action_mailer.delivery_method = :smtp
-  host = '<your heroku app>.herokuapp.com'
+  host = 'localhost:3000'
   config.action_mailer.default_url_options = { host: host }
   ActionMailer::Base.smtp_settings = {
     :address        => 'smtp.sendgrid.net',
@@ -74,7 +74,7 @@ Rails.application.configure do
     :authentication => :plain,
     :user_name      => ENV['SENDGRID_USERNAME'],
     :password       => ENV['SENDGRID_PASSWORD'],
-    :domain         => 'heroku.com',
+    :domain         => 'localhost:3000',
     :enable_starttls_auto => true
   }
 
