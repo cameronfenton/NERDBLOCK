8aa5b73076dc6ef16d0dfaf8035aed55f3ae2192
fentonca <fentonca@labprod.tsl.telus.com>
2017-12-21 05:16:21 -0500

    "You can actually subscribe to things, I wasn't expecting this to actually work"
    Signed-off-by: fentonca <fentonca@labprod.tsl.telus.com>

--- NERDBLOCK/app/controllers/subscriptions_controller.rb
+++ NERDBLOCK/app/controllers/subscriptions_controller.rb
@@ -1,28 +1,20 @@
 class SubscriptionsController < ApplicationController
   before_action :set_subscription, only: [:show, :edit, :update, :destroy]
 
-  # GET /subscriptions
-  # GET /subscriptions.json
   def index
     @subscriptions = Subscription.all
   end
 
-  # GET /subscriptions/1
-  # GET /subscriptions/1.json
   def show
   end
 
-  # GET /subscriptions/new
   def new
     @subscription = Subscription.new
   end
 
-  # GET /subscriptions/1/edit
   def edit
   end
 
-  # POST /subscriptions
-  # POST /subscriptions.json
   def create
     @subscription = Subscription.new(subscription_params)
 
@@ -37,8 +29,6 @@ class SubscriptionsController < ApplicationController
     end
   end
 
-  # PATCH/PUT /subscriptions/1
-  # PATCH/PUT /subscriptions/1.json
   def update
     respond_to do |format|
       if @subscription.update(subscription_params)
@@ -51,8 +41,6 @@ class SubscriptionsController < ApplicationController
     end
   end
 
-  # DELETE /subscriptions/1
-  # DELETE /subscriptions/1.json
   def destroy
     @subscription.destroy
     respond_to do |format|
@@ -62,13 +50,13 @@ class SubscriptionsController < ApplicationController
   end
 
   private
-    # Use callbacks to share common setup or constraints between actions.
+
     def set_subscription
       @subscription = Subscription.find(params[:id])
     end
 
-    # Never trust parameters from the scary internet, only allow the white list through.
+
     def subscription_params
-      params.permit(:option_id)
+      params.permit(:user_id, :genre_id, :option_id, :start_date, :end_date)
     end
 end
